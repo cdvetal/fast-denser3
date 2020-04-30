175729db0c4167394ac7c3c70a0204cac5576b72
cdv <fga@dei.uc.pt>
2020-04-30 20:17:58 +0100

    fix test utils

--- f-denser/tests/test_utils.py
+++ f-denser/tests/test_utils.py
@@ -32,7 +32,7 @@ class Test(unittest.TestCase):
 		return ind, grammar
 
 	def test_add_layer_random(self):
-		from fast_denser.f_denser import mutation
+		from fast_denser.engine import mutation
 
 		random.seed(0)
 		ind, grammar = self.create_individual()
@@ -46,7 +46,7 @@ class Test(unittest.TestCase):
 
 
 	def test_add_layer_replicate(self):
-		from fast_denser.f_denser import mutation
+		from fast_denser.engine import mutation
 
 		random.seed(0)
 		ind, grammar = self.create_individual()
@@ -60,7 +60,7 @@ class Test(unittest.TestCase):
 
 
 	def test_remove_layer(self):
-		from fast_denser.f_denser import mutation
+		from fast_denser.engine import mutation
 
 		random.seed(0)
 		ind, grammar = self.create_individual()
@@ -72,7 +72,7 @@ class Test(unittest.TestCase):
 		self.assertEqual(self.count_layers(ind.modules)-1, self.count_layers(new_ind.modules), "Error: remove layer wrong size")
 	
 	def test_mutate_ge(self):
-		from fast_denser.f_denser import mutation
+		from fast_denser.engine import mutation
 
 		random.seed(0)
 		ind, grammar = self.create_individual()
@@ -80,16 +80,23 @@ class Test(unittest.TestCase):
 		num_layers_before_mutation = len(ind.modules[0].layers)
 
 		new_ind = mutation(ind, grammar, 0, 0, 0, 0, 0, 1, 0, 0, 60)
+		# (individual, grammar, add_layer, re_use_layer, remove_layer, add_connection,\
+  #            remove_connection, dsge_layer, macro_layer, train_longer, default_train_time)
 
 		self.assertEqual(self.count_layers(ind.modules), self.count_layers(new_ind.modules), "Error: change ge parameter")
 
+		count_ref = list()
 		count_differences = 0
+		total_dif = 0
 		for module_idx in range(len(ind.modules)):
 			for layer_idx in range(len(ind.modules[module_idx].layers)):
+				total_dif += 1
 				if ind.modules[module_idx].layers[layer_idx] != new_ind.modules[module_idx].layers[layer_idx]:
-					count_differences += 1
+					if id(ind.modules[module_idx].layers[layer_idx]) not in count_ref:
+						count_ref.append(id(ind.modules[module_idx].layers[layer_idx]))
+						count_differences += 1
 
-		self.assertEqual(1, count_differences, "Error: change ge parameter")
+		self.assertEqual(total_dif, count_differences, "Error: change ge parameter")
 
 	
 	def test_keras_mapping(self):
@@ -104,7 +111,7 @@ class Test(unittest.TestCase):
 		keras_layers = evaluator.get_layers(phenotype)
 		model = evaluator.assemble_network(keras_layers, (32, 32, 3))
 
-		expected_output = {'layers': [{'class_name': 'InputLayer', 'config': {'dtype': 'float32', 'batch_input_shape': (None, 32, 32, 3), 'name': 'input_1', 'sparse': False}, 'inbound_nodes': [], 'name': 'input_1'}, {'class_name': 'Conv2D', 'config': {'kernel_constraint': None, 'kernel_initializer': {'class_name': 'VarianceScaling', 'config': {'distribution': 'normal', 'scale': 2.0, 'seed': None, 'mode': 'fan_in'}}, 'name': 'conv2d_1', 'bias_regularizer': None, 'bias_constraint': None, 'activation': 'linear', 'trainable': True, 'data_format': 'channels_last', 'padding': 'valid', 'strides': (2, 2), 'dilation_rate': (1, 1), 'kernel_regularizer': {'class_name': 'L1L2', 'config': {'l2': 0.0005000000237487257, 'l1': 0.0}}, 'filters': 90, 'bias_initializer': {'class_name': 'Zeros', 'config': {}}, 'use_bias': True, 'activity_regularizer': None, 'kernel_size': (4, 4)}, 'inbound_nodes': [[['input_1', 0, 0, {}]]], 'name': 'conv2d_1'}, {'class_name': 'Conv2D', 'config': {'kernel_constraint': None, 'kernel_initializer': {'class_name': 'VarianceScaling', 'config': {'distribution': 'normal', 'scale': 2.0, 'seed': None, 'mode': 'fan_in'}}, 'name': 'conv2d_2', 'bias_regularizer': None, 'bias_constraint': None, 'activation': 'sigmoid', 'trainable': True, 'data_format': 'channels_last', 'padding': 'same', 'strides': (2, 2), 'dilation_rate': (1, 1), 'kernel_regularizer': {'class_name': 'L1L2', 'config': {'l2': 0.0005000000237487257, 'l1': 0.0}}, 'filters': 95, 'bias_initializer': {'class_name': 'Zeros', 'config': {}}, 'use_bias': False, 'activity_regularizer': None, 'kernel_size': (5, 5)}, 'inbound_nodes': [[['conv2d_1', 0, 0, {}]]], 'name': 'conv2d_2'}, {'class_name': 'Flatten', 'config': {'trainable': True, 'name': 'flatten_1', 'data_format': 'channels_last'}, 'inbound_nodes': [[['conv2d_2', 0, 0, {}]]], 'name': 'flatten_1'}, {'class_name': 'Dense', 'config': {'kernel_initializer': {'class_name': 'VarianceScaling', 'config': {'distribution': 'normal', 'scale': 2.0, 'seed': None, 'mode': 'fan_in'}}, 'name': 'dense_1', 'kernel_constraint': None, 'bias_regularizer': None, 'bias_constraint': None, 'activation': 'softmax', 'trainable': True, 'kernel_regularizer': {'class_name': 'L1L2', 'config': {'l2': 0.0005000000237487257, 'l1': 0.0}}, 'bias_initializer': {'class_name': 'Zeros', 'config': {}}, 'units': 10, 'use_bias': True, 'activity_regularizer': None}, 'inbound_nodes': [[['flatten_1', 0, 0, {}]]], 'name': 'dense_1'}], 'input_layers': [['input_1', 0, 0]], 'output_layers': [['dense_1', 0, 0]], 'name': 'model_1'}
+		expected_output = {'name': 'model_1', 'layers': [{'name': 'input_1', 'class_name': 'InputLayer', 'config': {'batch_input_shape': (None, 32, 32, 3), 'dtype': 'float32', 'sparse': False, 'name': 'input_1'}, 'inbound_nodes': []}, {'name': 'conv2d_1', 'class_name': 'Conv2D', 'config': {'name': 'conv2d_1', 'trainable': True, 'dtype': 'float32', 'filters': 98, 'kernel_size': (5, 5), 'strides': (2, 2), 'padding': 'valid', 'data_format': 'channels_last', 'dilation_rate': (1, 1), 'activation': 'relu', 'use_bias': False, 'kernel_initializer': {'class_name': 'VarianceScaling', 'config': {'scale': 2.0, 'mode': 'fan_in', 'distribution': 'normal', 'seed': None}}, 'bias_initializer': {'class_name': 'Zeros', 'config': {}}, 'kernel_regularizer': {'class_name': 'L1L2', 'config': {'l1': 0.0, 'l2': 0.0005000000237487257}}, 'bias_regularizer': None, 'activity_regularizer': None, 'kernel_constraint': None, 'bias_constraint': None}, 'inbound_nodes': [[['input_1', 0, 0, {}]]]}, {'name': 'conv2d_2', 'class_name': 'Conv2D', 'config': {'name': 'conv2d_2', 'trainable': True, 'dtype': 'float32', 'filters': 104, 'kernel_size': (3, 3), 'strides': (1, 1), 'padding': 'valid', 'data_format': 'channels_last', 'dilation_rate': (1, 1), 'activation': 'sigmoid', 'use_bias': True, 'kernel_initializer': {'class_name': 'VarianceScaling', 'config': {'scale': 2.0, 'mode': 'fan_in', 'distribution': 'normal', 'seed': None}}, 'bias_initializer': {'class_name': 'Zeros', 'config': {}}, 'kernel_regularizer': {'class_name': 'L1L2', 'config': {'l1': 0.0, 'l2': 0.0005000000237487257}}, 'bias_regularizer': None, 'activity_regularizer': None, 'kernel_constraint': None, 'bias_constraint': None}, 'inbound_nodes': [[['conv2d_1', 0, 0, {}]]]}, {'name': 'flatten_1', 'class_name': 'Flatten', 'config': {'name': 'flatten_1', 'trainable': True, 'dtype': 'float32', 'data_format': 'channels_last'}, 'inbound_nodes': [[['conv2d_2', 0, 0, {}]]]}, {'name': 'dense_1', 'class_name': 'Dense', 'config': {'name': 'dense_1', 'trainable': True, 'dtype': 'float32', 'units': 10, 'activation': 'softmax', 'use_bias': True, 'kernel_initializer': {'class_name': 'VarianceScaling', 'config': {'scale': 2.0, 'mode': 'fan_in', 'distribution': 'normal', 'seed': None}}, 'bias_initializer': {'class_name': 'Zeros', 'config': {}}, 'kernel_regularizer': {'class_name': 'L1L2', 'config': {'l1': 0.0, 'l2': 0.0005000000237487257}}, 'bias_regularizer': None, 'activity_regularizer': None, 'kernel_constraint': None, 'bias_constraint': None}, 'inbound_nodes': [[['flatten_1', 0, 0, {}]]]}], 'input_layers': [['input_1', 0, 0]], 'output_layers': [['dense_1', 0, 0]]}
 
 		self.assertEqual(expected_output, model.get_config(), "Error: mapping")
 
