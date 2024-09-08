1234d4a9380f11bae4df520a52766f13e02241d6
Stefano Alberto Russo <stefano.russo@gmail.com>
2024-09-08 12:25:48 +0200

    Fixed bug in the example.sh script Docker image name.

--- docker/example.sh
+++ docker/example.sh
@@ -11,4 +11,4 @@ fi
 cd ../
 
 LD_PRELOAD="LD_PRELOAD=/usr/local/lib/python3.8/dist-packages/scikit_learn.libs/libgomp-d22c30c5.so.1.0.0"
-docker run -it -v$PWD:/code denser3 /bin/bash -c "cd /code/f-denser && $LD_PRELOAD DEBUG=True python3 -m fast_denser.engine -d mnist -c /code/example/config.json -r 1 -g /code/example/cnn.grammar"
+docker run -it -v$PWD:/code fast-denser3 /bin/bash -c "cd /code/f-denser && $LD_PRELOAD DEBUG=True python3 -m fast_denser.engine -d mnist -c /code/example/config.json -r 1 -g /code/example/cnn.grammar"
